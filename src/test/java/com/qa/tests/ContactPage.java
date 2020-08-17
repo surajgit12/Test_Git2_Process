ee1d4aa6b0163ca2f479b001bcd1b4da05bb3e51
surajgit12 <surajgit12@gmail.com>
2020-08-17 17:19:37 +0530

    added contacts page code

---
+++ src/test/java/com/qa/tests/ContactPage.java
@@ -0,0 +1,14 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class ContactPage {
+
+	@Test(priority = 0)
+	void Contacts_Page() {
+
+		System.out.println("Contacts_Page");
+
+	}
+
+}
