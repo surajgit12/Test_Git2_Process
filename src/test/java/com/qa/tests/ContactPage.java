babced705a0f9a1783cd167be002fe58c18d1dbd
surajgit12 <surajgit12@gmail.com>
2020-08-17 17:39:29 +0530

    added contact page code for testng

--- src/test/java/com/qa/tests/ContactPage.java
+++ src/test/java/com/qa/tests/ContactPage.java
@@ -1,5 +1,6 @@
 package com.qa.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ContactPage {
@@ -8,7 +9,19 @@ public class ContactPage {
 	void Contacts_Page() {
 
 		System.out.println("Contacts_Page");
+		
+		Assert.assertTrue(true);
 
 	}
+	
+	@Test(priority = 0)
+	void Contacts_for_Mailing_Address() {
+
+		System.out.println("Contacts_for_Mailing_Address_Page");
+		
+		Assert.assertTrue(true);
+
+	}
+
 
 }
